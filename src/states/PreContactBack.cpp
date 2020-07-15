804de14035a704172868fb0c48f0a44221d5a61f
nana <bolotnikova.anastasia@gmail.com>
2020-07-15 18:31:56 +0200

    Look at hand trajectory task end point in PreContactBack state

--- src/states/PreContactBack.cpp
+++ src/states/PreContactBack.cpp
@@ -18,13 +18,6 @@ void PreContactBack::start(mc_control::fsm::Controller & ctl_)
     ctl.processGrippers(config_("grippers"));
   }
 
-  // Load camera orientation task
-  if(!config_.has("lookAtHandTarget")){
-    mc_rtc::log::error_and_throw<std::runtime_error>("PreContactBack start | lookAtHandTarget config entry missing");
-  }
-  lookAtHandTarget_ = mc_tasks::MetaTaskLoader::load<mc_tasks::LookAtTask>(ctl_.solver(), config_("lookAtHandTarget"));
-  ctl_.solver().addTask(lookAtHandTarget_);
-
   // Load hand trajectory task and completion criteria
   if(!config_.has("handTrajectoryTask")){
     mc_rtc::log::error_and_throw<std::runtime_error>("PreContactBack start | handTrajectoryTask config entry missing");
@@ -34,7 +27,6 @@ void PreContactBack::start(mc_control::fsm::Controller & ctl_)
     mc_rtc::log::error_and_throw<std::runtime_error>("PreContactBack start | completion config entry missing for handTrajectoryTask");
   }
   trajTaskCriteria_.configure(*handTrajectoryTask_, ctl_.solver().dt(), config_("handTrajectoryTask")("completion"));
-
   // Adjust trajectory task bspline end point Z axis value to the human height
   Eigen::Vector3d target = handTrajectoryTask_->spline().target();
   if(!std::isnan(ctl.humanUpperBackLevel())){
@@ -45,9 +37,19 @@ void PreContactBack::start(mc_control::fsm::Controller & ctl_)
     target(2) = ctl.chairSeatHeight() + 0.3*ctl.humanHeight();
   }
   handTrajectoryTask_->spline().target(target);
-
   // Add hand trajectory task to the solver
   ctl_.solver().addTask(handTrajectoryTask_);
+
+  // Load camera orientation task
+  if(!config_.has("lookAtHandTarget")){
+    mc_rtc::log::error_and_throw<std::runtime_error>("PreContactBack start | lookAtHandTarget config entry missing");
+  }
+  lookAtHandTarget_ = mc_tasks::MetaTaskLoader::load<mc_tasks::LookAtTask>(ctl_.solver(), config_("lookAtHandTarget"));
+  if(!config_("lookAtHandTarget").has("targetPos")){
+    // Look at (adjusted) hand trajectory task target
+    lookAtHandTarget_->target(target);
+  }
+  ctl_.solver().addTask(lookAtHandTarget_);
 }
 
 bool PreContactBack::run(mc_control::fsm::Controller & ctl_)
